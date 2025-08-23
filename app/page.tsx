78bc0e1574c7100620595fb99aa68a96e56757bc
Felix Wani <lokosangwani21@Gmail.com>
2025-08-23 21:49:55 +0200

    configured and tested shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { IconGitBranch } from "@tabler/icons-react";
 
 export default function Homepage() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex flex-col gap-5 p-10">
+      <h1>Home</h1>
+      <Input className="w-max" placeholder="input test" />
+      <Button className="w-max" variant={"default"}>
+        Button test
+      </Button>
+      <Button className="w-max" variant={"outline"}>
+        {" "}
+        <IconGitBranch />
+        Button test
+      </Button>
     </div>
   );
 }
