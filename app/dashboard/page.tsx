773bfb412a2f323d39c931234f362166505ccb95
Felix Wani <lokosangwani21@Gmail.com>
2025-09-02 21:54:41 +0200

    clerk auth intial setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,9 @@ export default function Dashboard() {
   return (
     <>
       <div>
-        <h1>User Dashboard</h1>
+        <h1>User Dashboard
+          
+        </h1>
       </div>
     </>
   );
