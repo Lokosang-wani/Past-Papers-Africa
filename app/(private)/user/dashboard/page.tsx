cacc338e8581e7f40d0f16779895596b4419350a
Felix Wani <lokosangwani21@Gmail.com>
2025-09-03 00:45:20 +0200

    acess user data from clerk and render locally

--- app/(private)/user/dashboard/page.tsx
+++ app/(private)/user/dashboard/page.tsx
@@ -1,9 +1,31 @@
-export default function UserDashboardPage() {
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+async function UserDashboardPage() {
+  const user = await currentUser();
+
   return (
     <>
-      <div>
-        <h1>User Dashboard</h1>
+      <div className="p-5 flex flex-col gap-5">
+        <h1>User Dashboard Page</h1>
+        <UserButton />
+
+        <div className="flex flex-col gap-2">
+          <h1>clerk User ID : {user?.id}</h1>
+          <h1>Clerk User Email : {user?.emailAddresses[0].emailAddress}</h1>
+          <h1>User Full Name: {user?.firstName + " " + user?.lastName}</h1>
+          <h1>Username : {user?.username}</h1>
+          <h1>
+            User Profile Picture :{" "}
+            <img
+              src={user?.imageUrl}
+              alt="user profile picture"
+              className="w-20 h-20 rounded-full"
+            />{" "}
+          </h1>
+        </div>
       </div>
     </>
   );
 }
+
+export default UserDashboardPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useState } from "react";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -38,8 +39,16 @@ export default function Homepage() {
     <>
       <div className="lg:px-20 px-5 py-10">
         <div className="flex justify-between items-center">
-          <h1 className="font-extrabold  text-3xl text-primary">
-            <b>P.P.A</b>
+          <h1 className="flex items-center gap-2 font-extrabold text-3xl text-primary">
+            {" "}
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={45}
+              height={45}
+              className="filter grayscale hover:grayscale-0 transition duration-300"
+            />
+            <span>P.P.A</span>
           </h1>
 
           <div className="flex gap-5 items-center">
@@ -58,13 +67,23 @@ export default function Homepage() {
         </div>
 
         <div className="text-center min-h-[70vh]">
-          <h1 className="text-4xl font-semibold tracking-tight text-balance text-gray-800 sm:text-5xl mt-8">
+          <h1 className="text-4xl font-semibold tracking-tight text-balance text-gray-800 sm:text-5xl mt-8 pt-8">
             Master African School Certificates Exams Online
           </h1>
-          <p className="mt-4 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+          <p className="mt-4 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8 pb-5">
             Practice past papers, take timed exams, and get instant AI-powered
             feedback.
           </p>
+
+          <div className="pt-8 justify-center flex">
+            <Image
+              src="/sweating.jpg" // Image in the public folder
+              alt="Logo"
+              width={800}
+              height={800}
+              className="filter grayscale hover:grayscale-0 transition duration-300 rounded-2xl"
+            />
+          </div>
         </div>
         <Sheet open={openSignInForm} onOpenChange={setOpenSignInForm}>
           <SheetContent className="min-w-[500px]">
