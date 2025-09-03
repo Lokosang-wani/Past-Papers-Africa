b8b1eec72a2281529b48ca1ee975c8e8bb857945
Felix Wani <lokosangwani21@Gmail.com>
2025-09-03 22:16:44 +0200

    supabase intial setup done

---
+++ config/supabase-config.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://pbgrcomdcpkpylnocupb.supabase.co'
+const supabaseKey = process.env.SUPABASE_API_KEY || ''
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+export default supabase;
