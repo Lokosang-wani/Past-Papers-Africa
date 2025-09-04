cd6062bb9494e0091268eedacdadf60b9193e576
Felix Wani <lokosangwani21@Gmail.com>
2025-09-04 14:54:30 +0200

    added dashboard stats components

--- app/(private)/user/dashboard/page.tsx
+++ app/(private)/user/dashboard/page.tsx
@@ -1,6 +1,15 @@
 import { getCurrentUserFromSupabase } from "@/actions/users";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 async function UserDashboardPage() {
   const user = await currentUser();
   const supabaseUserResponse = await getCurrentUserFromSupabase();
@@ -15,19 +24,44 @@ async function UserDashboardPage() {
         <div className="flex flex-col gap-2 ">
           <h1 className="text-3xl font-semibold text-gray-800 sm:text-5xl">Welcome back, {user?.firstName + " " + user?.lastName}</h1>
           <p className="text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Ready to continue your exam preparation journey?</p>
-          {/* <h1>clerk User ID : {user?.id}</h1>
-          <h1>Clerk User Email : {user?.emailAddresses[0].emailAddress}</h1>
-          <h1>User Full Name: {user?.firstName + " " + user?.lastName}</h1>
-          <h1>Username : {user?.username}</h1>
-          <h1>
-            User Profile Picture :{" "}
-            <img
-              src={user?.imageUrl}
-              alt="user profile picture"
-              className="w-20 h-20 rounded-full"
-            />{" "}
-          </h1> */}
         </div>
+        <div className="grid grid-cols-3 gap-8">
+               <Card className="">
+            <CardHeader>
+              <div className="">
+              <CardTitle>Total Exams</CardTitle>
+              <CardContent className="text-4xl">12</CardContent>
+              <CardDescription>Exams completed</CardDescription>
+              </div>
+              
+            </CardHeader>
+          </Card>
+          <Card className="">
+            <CardHeader>
+              <div className="">
+              <CardTitle>Average Score</CardTitle>
+              <CardContent className="text-4xl">78%</CardContent>
+              <CardDescription>Across all exams</CardDescription>
+              </div>
+              
+            </CardHeader>
+          </Card>
+          <Card className="">
+            <CardHeader>
+              <div className="">
+              <CardTitle>Improvement</CardTitle>
+              <CardContent className="text-4xl">+15%</CardContent>
+              <CardDescription>Exams completed</CardDescription>
+              </div>
+              
+            </CardHeader>
+          </Card>
+          
+          </div>
+          <div>
+            <h1 className="text-3xl font-semibold text-gray-800 sm:text-3xl">Filter Exams below</h1>
+            <p className="text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Choose country, certificate type, year and subject</p>
+          </div>
       </div>
     </>
   );
